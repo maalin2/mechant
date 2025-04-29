9f1273106a951573f2edf3d18db98b242ee8fef8
mo <1maalinma@gmail.com>
2025-04-29 13:47:57 -0400

    add mit license; print alphavantage_api

--- mechant/bin/main.ml
+++ mechant/bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let () = 
+        print_endline "Hello, World!";
+        let env = Dotenv.parse() in
+        List.iter (fun (name, value) -> 
+                print_endline (Printf.sprintf "%s=%s" name value)
+        ) env
