cf038c2a3560baa037c1ee284132aac3e1350716
maalin2 <151789225+maalin2@users.noreply.github.com.>
2025-05-01 16:31:04 -0400

    quote url

--- mechant/bin/main.ml
+++ mechant/bin/main.ml
@@ -1,5 +1,9 @@
 let () = 
-        let env = Dotenv.parse ~debug: true () in
-        List.iter (fun (name, value) -> 
-                print_endline (Printf.sprintf "%s=%s" name value)
-        ) env;
+        Dotenv.export ~debug: true () |> ignore;
+        let symbol = "AAPL" in
+        match Sys.getenv_opt "ALPHA_KEY" with
+        | Some api_key ->
+                let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" ^ symbol ^ "&apikey=" ^ api_key in
+                print_endline ("url below\n" ^ url)
+        | None ->
+                print_string "no api key\n"
