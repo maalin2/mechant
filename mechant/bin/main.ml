ba7b4d37ad2f19f5787576ced6bf6f93448f31f3
maalin2 <151789225+maalin2@users.noreply.github.com.>
2025-05-01 15:51:17 -0400

    use debug flag

--- mechant/bin/main.ml
+++ mechant/bin/main.ml
@@ -1,6 +1,5 @@
 let () = 
-        print_endline "Hello, World!";
-        let env = Dotenv.parse() in
+        let env = Dotenv.parse ~debug: true () in
         List.iter (fun (name, value) -> 
                 print_endline (Printf.sprintf "%s=%s" name value)
-        ) env
+        ) env;
