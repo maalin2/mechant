f43a094045795a49d0ebd69e0f36404bb8614be0
maalin2 <151789225+maalin2@users.noreply.github.com.>
2025-05-02 19:57:36 -0400

    modularize for testing

---
+++ mechant/bin/alpha_api.ml
@@ -0,0 +1,22 @@
+open Cohttp_lwt_unix
+open Yojson.Basic.Util
+open Lwt.Infix
+
+let build_url ~api_key ~symbol = 
+        "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" ^
+        symbol ^ "&apikey=" ^ api_key
+
+let fetch_price ~api_key ~symbol = 
+        let url = build_url ~api_key ~symbol in
+        Client.get (Uri.of_string url) >>= fun (_, body) ->
+        Cohttp_lwt.Body.to_string body >|= fun body_str ->
+
+        (* json time *)
+        let json = Yojson.Basic.from_string body_str in
+        let global_quote = json |> member "Global Quote" in
+        global_quote |> member "05. price" |> to_string
+
+
+        
+
+
--- mechant/bin/main.ml
+++ mechant/bin/main.ml
@@ -1,25 +1,16 @@
-open Cohttp_lwt_unix
-open Yojson.Basic.Util
 open Lwt.Infix
 
 let () = 
         Dotenv.export ~debug: true () |> ignore;
-        let symbol = "AAPL" in
         match Sys.getenv_opt "ALPHA_KEY" with
                 | Some api_key ->
-                        begin
-                                let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" ^ symbol ^ "&apikey=" ^ api_key in
-                                print_endline ("url below\n" ^ url);
-
-                                let main =
-                                        Client.get (Uri.of_string url) >>= fun (_, body) ->
-                                        body |> Cohttp_lwt.Body.to_string >|= fun body_str ->
-                                        let json = Yojson.Basic.from_string body_str in
-                                        let global_quote = json |> member "Global Quote" in
-                                        let price = global_quote |> member "05. price" |> to_string in
-                                        print_endline ("price of " ^ symbol ^ " is " ^  price)
-                                in
-                                Lwt_main.run main
-                        end
+                        let symbol = "AAPL" in
+                        let main = 
+                                Alpha_api.fetch_price ~api_key ~symbol >|= fun
+                                        price ->
+                                print_endline ("price of " ^ symbol ^ " is " ^
+                                price)
+                        in
+                        Lwt_main.run main
                 | None ->
                         print_string "no api key\n"
